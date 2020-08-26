f8b473de3d20b72126b5143ca6b11e331a3e060a
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2020-08-26 11:09:30 -0500

    Update crear_corte.php

--- php/crear_corte.php
+++ php/crear_corte.php
@@ -53,13 +53,7 @@ if ($Pass['pass'] == $Clave){
                 setTimeout("location.href='cortes_pagos.php'", 2000);
             </script>
             <?php
-            #SI ALMENOS UNO DE LOS TOTALES ES MAYOR A CERO PROCEDEMOS A ENVIAR EL MENSAJE POR TELEGRAM
-            $Ping = shell_exec("ping 8.8.8.8");//SENTENCIA QUE SIRVE PARA HACER PING MEDIANTE LA CONSOLA DE LA PC
-            #COMPARAMOS LOS RESULTADOS DEL PING ECHO SI HACE PING SIN PERDIDAS A LA DIRECCION DE GOOGLE PARA COMPROBAR QUE ALLA INTERNET
-            if (strpos($Ping, "perdidos = 0") AND strpos($Ping, "Respuesta desde 8.8.8.8: bytes=32")) {
-                #SI HACE PING SI HABRA INTERNET Y POR TANTO NO MARCARA ERROR INCLUIMOS EL ARCHIVO QUE ENVIA EL MSJ  
-                include ('msj_botTelegramCorte.php');                  
-            }
+            include ('msj_botTelegramCorte.php');                  
         }
     }
 }else{
