01a0fc0b9454c30b0a6842a363dfea8d02759a11
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2020-09-14 17:05:48 -0500

    Update buscar_cortes_full2.php

--- php/buscar_cortes_full2.php
+++ php/buscar_cortes_full2.php
@@ -1,4 +1,5 @@
 <?php
+include('is_logged.php');
 
 include('../php/conexion.php');
 date_default_timezone_set('America/Mexico_City');
@@ -13,6 +14,8 @@ echo "/ip firewall address-list <br>";
 $ARRAYCORTADOS = mysqli_query($conn, "SELECT * FROM clientes INNER JOIN comunidades ON clientes.lugar = comunidades.id_comunidad WHERE clientes.fecha_corte < '$hoy' AND clientes.instalacion = 1 AND comunidades.servidor = $Servidor");
 
 while ($cortes = mysqli_fetch_array($ARRAYCORTADOS)) {
+	#AGREGAMOS LA IP A LISTA DE MOROSOS
+            	$id_user = $_SESSION['user_id'];
 	$usuario = mysqli_fetch_array(mysqli_query($conn,"SELECT * FROM users WHERE user_id = $id_user"));
     $comment = 'No_Cliente: '.$cortes['id_cliente'].' Cortado por: '.$usuario['firstname'];
 	echo "add address= ".$cortes['ip']." comment= ".$comment]." list= MOROSOS <br>";
