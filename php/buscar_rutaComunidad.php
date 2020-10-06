b6bd80b8fa336aa462963e4beffb65670e310dba
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2020-10-06 13:29:49 -0500

    Update buscar_rutaComunidad.php

--- php/buscar_rutaComunidad.php
+++ php/buscar_rutaComunidad.php
@@ -12,6 +12,8 @@ $Texto = $conn->real_escape_string($_POST['texto']);
 if ($Texto != "") {
 	#SI RECIBIMOS ALGUN TEXTO BUSCAMOS ALGUNA COMUNIDAD CON ESE NOMBRE SIMILAR
 	$sql = "SELECT * FROM comunidades WHERE nombre LIKE '%$Texto%'";
+	echo '<script>M.toast({html:"Texto.", classes: "rounded"})</script>';
+
 }else{
 	#SI EL TEXTO ESTA VACIO SELECCIONAMOS TODAS LAS COMUNIDADES
 	$sql = "SELECT * FROM comunidades";
