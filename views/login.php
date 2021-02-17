e21af91c855010f4a1ca3bda911049687871e01f
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2021-02-17 10:24:24 -0600

    Update login.php

--- views/login.php
+++ views/login.php
@@ -32,7 +32,7 @@ if ($login->isUserLoggedIn() == true){
 <html lang="es">
 <head>
   <!-- FONDO DE PANTALLA DE LOGIN -->  
-  <img id="video_background"  src="../img/home.jpg">
+  <img id="video_background"  src="../img/home.jpg"><br><br><br><br>
 
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
