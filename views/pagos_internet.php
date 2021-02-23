89c477269fefeae812f7ffbcf1b1db5f842c201a
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2021-02-23 13:48:39 -0600

    correcion

--- views/pagos_internet.php
+++ views/pagos_internet.php
@@ -367,7 +367,7 @@ $area = mysqli_fetch_array(mysqli_query($conn, "SELECT area FROM users WHERE use
           <option value="2019">2019</option>
           <option value="2020">2020</option>
           <option value="2021">2021</option>
-          <option value="2021">2022</option>
+          <option value="2022">2022</option>
         </select>
       </div>
       <div class="col s4 m2 l2">
