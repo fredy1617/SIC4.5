0fe7d3e48e7f8247c1d8f42de9310fba8d02afe6
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2021-03-10 09:39:23 -0600

    Create responde_bot.php

---
+++ php/responde_bot.php
@@ -0,0 +1,30 @@
+<?php
+$token = '1652202112:AAHyH8NzDJfP4XIMSUXNv-04Se7zORNLnkI';
+$website = 'https://api.telegram.org/bot'.$token;
+
+$input = file_get_contents('php://input');
+$update = json_decode($input, TRUE);
+
+$chatId = $update['message']['chat']['id'];
+$message = $update['message']['text'];
+
+switch($message) {
+    case '/start':
+        $response = 'Me has iniciado';
+        sendMessage($chatId, $response);
+        break;
+    case '/info':
+        $response = 'Hola! Soy @trecno_bot';
+        sendMessage($chatId, $response);
+        break;
+    default:
+        $response = 'No te he entendido';
+        sendMessage($chatId, $response);
+        break;
+}
+
+function sendMessage($chatId, $response) {
+    $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
+    file_get_contents($url);
+}
+?>
