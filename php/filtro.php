a8774111ab409a1cdbd38620cd170c575b867c5d
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2021-04-26 13:21:49 -0500

    Update filtro.php

--- php/filtro.php
+++ php/filtro.php
@@ -1,5 +1,5 @@
 <?php
-$array = array('172.16.12.101','alguna ip mas');
+$array = array('172.16.12.101','172.16.189.120');
 foreach ($array as &$valor) {
     if ($IP_M == $valor){
     	$filtro = true;
