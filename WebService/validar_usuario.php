ea5a232aa1f866eb6028ebb66c0e1154d153c2d2
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2021-06-08 17:20:58 -0500

    Revert "Update validar_usuario.php"
    This reverts commit 896a3cf236591b948f351fc341a2274a00263991.

--- WebService/validar_usuario.php
+++ WebService/validar_usuario.php
@@ -3,20 +3,29 @@
 include('../php/conexion.php');
 include_once("../php/password_compatibility_library.php");
 
-
 $usu_usuario=$_POST['usuario'];//$usu_usuario='Juanito';
 $usu_password=$_POST['password'];//$usu_password='12345678';
-$valorUserPassword_hash = password_hash($valorUserPassword, PASSWORD_DEFAULT);
 
-echo $usu_password;
-$sentencia=$conn->prepare("SELECT * FROM users WHERE user_name=? AND user_password_hash=?");
-$sentencia->bind_param('ss',$usu_usuario,$valorUserPassword_hash);
-$sentencia->execute();
+$sql = "SELECT user_id, user_name, user_email, user_password_hash
+        FROM users
+        WHERE user_name = '" . $usu_usuario . "' OR user_email = '" . $usu_usuario . "';";
+$result_of_login_check = $this->conn->query($sql);
+if ($result_of_login_check->num_rows == 1) {
+    // get result row (as an object)
+    $result_row = $result_of_login_check->fetch_object();
 
-$resultado = $sentencia->get_result();
-if ($fila = $resultado->fetch_assoc()) {
-  echo json_encode($fila,JSON_UNESCAPED_UNICODE);
+    // using PHP 5.5's password_verify() function to check if the provided password fits
+    // the hash of that user's password
+    if (password_verify($usu_password, $result_row->user_password_hash)) {
+      // write user data into PHP SESSION (a file on your server)
+      
+    } else {
+      echo "Usuario y/o contraseña no coinciden.";
+    }
+} else {
+      echo "Usuario y/o contraseña no coinciden.";
 }
+#$valorUserPassword_hash = password_hash($usu_password, PASSWORD_DEFAULT);
 $sentencia->close();
 $conn->close();
 ?>
