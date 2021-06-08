f7164af9555f7e109fb44b106902d52d391c6b3a
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2021-06-08 08:51:25 -0500

    Update pagos_internet.php

--- views/pagos_internet.php
+++ views/pagos_internet.php
@@ -373,9 +373,7 @@ $area = mysqli_fetch_array(mysqli_query($conn, "SELECT area FROM users WHERE use
         <select id="año" class="browser-default">
           <option value="0" selected>Seleccione Año</option>
           <option value="2020">2020</option>
-          <option value="2021">2021</option>       
-          <option value="2022">2022</option>       
-          <option value="2023">2023</option>       
+          <option value="2021">2021</option>        
         </select>
       </div>
       <div class="col s4 m2 l2">
