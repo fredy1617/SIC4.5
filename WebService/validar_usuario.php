e2eced525eef0579e031828e21f89c07f8b1fa27
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2021-06-08 17:24:58 -0500

    Update validar_usuario.php

--- WebService/validar_usuario.php
+++ WebService/validar_usuario.php
@@ -1,12 +1,14 @@
 <?php
 #INCLUIMOS EL ARCHIVO CON LA CONEXION A LA BASE DE DATOS
 include('../php/conexion.php');
+include_once("../php/password_compatibility_library.php");
 
 $usu_usuario=$_POST['usuario'];//$usu_usuario='Juanito';
-$usu_password=$_POST['password'];//$usu_password='12345678';
+$usu_password=$_POST['password'];//$usu_password='12345678';0
+$valorUserPassword_hash = password_hash($valorUserPassword, PASSWORD_DEFAULT);
 
-$sentencia=$conn->prepare("SELECT * FROM users WHERE user_name=? AND user_name=?");
-$sentencia->bind_param('ss',$usu_usuario,$usu_password);
+$sentencia=$conn->prepare("SELECT * FROM users WHERE user_name=? AND user_password_hash=?");
+$sentencia->bind_param('ss',$usu_usuario,$valorUserPassword_hash);
 $sentencia->execute();
 
 $resultado = $sentencia->get_result();
