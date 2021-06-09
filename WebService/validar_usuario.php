591d975909f051e1fe865461d5e0ebbbc5155db3
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2021-06-09 08:36:57 -0500

    Update validar_usuario.php

--- WebService/validar_usuario.php
+++ WebService/validar_usuario.php
@@ -4,8 +4,7 @@ include('../php/conexion.php');
 include_once("../php/password_compatibility_library.php");
 
 $usu_usuario=$_POST['usuario'];//$usu_usuario='Juanito';
-$usu_password=$_POST['password'];//$usu_password='12345678';0
-$valorUserPassword_hash = password_hash($usu_password, PASSWORD_DEFAULT);
+$usu_password=$_POST['password'];//$usu_password='12345678';
 
 $sentencia=$conn->prepare("SELECT * FROM users WHERE user_name=?");
 $sentencia->bind_param('s',$usu_usuario);
@@ -13,7 +12,13 @@ $sentencia->execute();
 
 $resultado = $sentencia->get_result();
 if ($fila = $resultado->fetch_assoc()) {
-  //echo json_encode($fila,JSON_UNESCAPED_UNICODE);
+  if (password_verify($usu_password, $resultado->user_password_hash)) {
+    echo json_encode($fila,JSON_UNESCAPED_UNICODE);
+  } else {
+    echo "Usuario y/o contraseña no coinciden.";
+  }
+} else {
+  echo "Usuario y/o contraseña no coinciden.";
 }
 $sentencia->close();
 $conn->close();
