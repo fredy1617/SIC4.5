a057ec57a35d60e7efe016c612a97de37eae539f
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2021-07-09 08:20:14 -0500

    Update superAdmin.php

--- php/superAdmin.php
+++ php/superAdmin.php
@@ -3,7 +3,7 @@ include('conexion.php');
 $id = $_SESSION['user_id'];
 $area = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM users WHERE user_id=$id"));
 
-if($area['area']=="Administrador" AND ($area['user_id'] == 10 OR $area['user_id']==49 OR $area['user_id']==25 OR $area['user_id']==75)){
+if($area['area']=="Administrador" AND ($area['user_id'] == 10 OR $area['user_id']==49 OR $area['user_id']==25 OR $area['user_id']==75 OR $area['user_id']==83)){
 }else{
 	echo '<script>M.toast({html:"Permiso denegado. Direccionando a la página principal.", classes: "rounded"})</script>';
   	echo '<script>admin();</script>';
