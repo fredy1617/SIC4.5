fc4a68d0a02144a1b445641074baabefdbb4276b
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2021-08-02 15:50:22 -0500

    Update pagos_telefono.php

--- views/pagos_telefono.php
+++ views/pagos_telefono.php
@@ -204,7 +204,7 @@ $area = mysqli_fetch_array(mysqli_query($conn, "SELECT area FROM users WHERE use
             <option value="2021">2021</option>
           </select>
         </div>  
-        <?php if ($user_id == 38 OR $user_id == 10 OR $user_id == 77 OR $user_id == 49 OR $user_id == 70) { 
+        <?php if ($user_id == 38 OR $user_id == 10 OR $user_id == 77 OR $user_id == 49 OR $user_id == 70  OR $user_id == 75) { 
            $Ser = '';
         }else{ $Ser = 'disabled="disabled"';}?>
         <div class="col s6 m1 l1">
