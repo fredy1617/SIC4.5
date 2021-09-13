522d87bd04713887a6a09c608ec2ad4c1d565ba1
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2021-09-13 17:14:48 -0500

    Update editar_cliente.php

--- views/editar_cliente.php
+++ views/editar_cliente.php
@@ -153,7 +153,7 @@ $paquete_cliente = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM paquete
                 $sql = mysqli_query($conn,"SELECT * FROM comunidades ORDER BY nombre");
                 while($comunidad = mysqli_fetch_array($sql)){
                   ?>
-                    <option value="<?php echo $comunidad['id_comunidad'];?>"><?php echo $comunidad['nombre'];?> - $<?php echo $comunidad['instalacion'];?></option>
+                    <option value="<?php echo $comunidad['id_comunidad'];?>"><?php echo $comunidad['nombre'].', '.$comunidad['municipio'];?> - $<?php echo $comunidad['instalacion'];?></option>
                   <?php
                 } 
             ?>
