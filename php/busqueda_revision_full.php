3a3b621750e10cb6fcb03c21d702922ead02c99b
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2021-09-15 12:05:12 -0500

    revisar cliente por cliente

--- php/busqueda_revision_full.php
+++ php/busqueda_revision_full.php
@@ -2,8 +2,8 @@
 		include('../php/conexion.php');
 		$index = $conn->real_escape_string($_POST['valorIndex']);
 
-		$inicia = 500 * ($index-1);
-		$hasta = 500;
+		$inicia = 250 * ($index-1);
+		$hasta = 250;
 
 		$sql = mysqli_query($conn, "SELECT * FROM clientes  ORDER BY id_cliente LIMIT $inicia,$hasta");
 		$filas = mysqli_num_rows($sql);
@@ -13,19 +13,19 @@
 			while ($cliente = mysqli_fetch_array($sql)) {
 				$id_comunidad = $cliente['lugar'];
 				$comunidad = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM comunidades WHERE id_comunidad = '$id_comunidad'"));
-				$id_servidor = $comunidad['servidor'];
-				$Servidor = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM servidores WHERE id_servidor = '$id_servidor'"));
 				$id_cliente = $cliente['id_cliente'];
-				$pago = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM pagos WHERE id_cliente = '$id_cliente' AND tipo = 'Mensualidad' Order by id_pago DESC"));
+				$pago =  mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM pagos WHERE id_cliente = $id_cliente AND tipo = 'Mensualidad' ORDER BY id_pago DESC LIMIT 1,1"));
+				$pago2 =  mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM pagos WHERE id_cliente = $id_cliente AND tipo = 'Mensualidad' ORDER BY id_pago DESC LIMIT 2,2"));
+
 		?>
 			<tr>
 				<td><?php echo $id_cliente; ?></td>
 				<td><?php echo $cliente['nombre']; ?></td>
-				<td><?php echo $cliente['fecha_corte']; ?></td>
-				<td><?php echo $pago['descripcion']; ?></td>
-				<td><?php echo $pago['cantidad']; ?></td>
+				<td><?php echo '<b>PENULTIMO: </b>$'.$pago2['cantidad'].' '.$pago2['descripcion'].'<br><b>ULTIMO: </b>$'.$pago['cantidad'].' '.$pago['descripcion']; ?></td>
+				<td width="110px"><b><?php echo $cliente['fecha_corte']; ?></b></td>
 				<td><?php echo $cliente['ip']; ?></td>
-				<td><?php echo $Servidor['nombre']; ?></td>
+				<td><b><?php echo $cliente['servicio']; ?></b></td>
+				<td><?php echo $comunidad['nombre']; ?></td>
 				<td><form method="post" action="../views/editar_revision.php"><input name="id_cliente" type="hidden" value="<?php echo $id_cliente; ?>"><button type="submit" class="btn-floating btn-tiny waves-effect waves-light pink"><i class="material-icons">edit</i></button></form></td>
 			</tr>
 		<?php
--- views/revision_full.php
+++ views/revision_full.php
@@ -34,6 +34,20 @@ include ('../php/cobrador.php');
 	            <option value="4">4</option>
 	            <option value="5">5</option>
 	            <option value="6">6</option>	            
+	            <option value="7">7</option>	            
+	            <option value="8">8</option>	            
+	            <option value="9">9</option>	            
+	            <option value="10">10</option>	            
+	            <option value="11">11</option>	            
+	            <option value="12">12</option>	            
+	            <option value="13">13</option>	            
+	            <option value="14">14</option>	            
+	            <option value="15">15</option>	            
+	            <option value="16">16</option>	            
+	            <option value="17">17</option>	            
+	            <option value="18">18</option>	            
+	            <option value="19">19</option>	            
+	            <option value="20">20</option>	            
 	          </select>
 	        </div>
 	        </div>
@@ -41,16 +55,16 @@ include ('../php/cobrador.php');
 		        <a onclick="busqueda();" class="waves-effect waves-light btn pink left right"><i class="material-icons center">send</i></a>
 		    </div>
 		</div>
-		<table class="bordered  highlight responsive-table">
+		<table class="bordered highlight responsive-table">
 		<thead>
 			<tr>
 				<th>No.Cliente</th>
 				<th>Nombre</th>
-				<th>Fecha de Corte</th>
-				<th>Utimo Pago</th>
-				<th>Cantidad</th>
+				<th>Pagos</th>
+				<th width="110px">Fecha de Corte</th>
 				<th>Ip</th>
 				<th>Servidor</th>
+				<th>Comunidad</th>
 				<th>Editar</th>
 			</tr>
 		</thead>	
